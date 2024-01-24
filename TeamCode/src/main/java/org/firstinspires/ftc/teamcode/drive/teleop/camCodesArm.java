f5b05df89a8d4858c98f3e2606745c8c2ad45910
Cameron <Cameronfrancini@chatham-nj.org>
2024-01-24 15:57:21 -0500

    I added code that starts to set up to allow it to move the arm... In theory...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/camCodesArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/camCodesArm.java
@@ -1,23 +1,39 @@
 package org.firstinspires.ftc.teamcode.drive.teleop;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 
-@TeleOp( name = "Cam Codes a Scuffed Teleop--> vroom")
-public class camCodes extends LinearOpMode {
+@TeleOp( name = "Cam Codes a Scuffed Teleop--> Arm")
+public class camCodesArm extends LinearOpMode {
 
     SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
+    DcMotorEx armPivot = null;
     private double basePower = 0.4;
+
+
+
     @Override
     public void runOpMode() throws InterruptedException {
 
+
+        armPivot = hardwareMap.get(DcMotorEx.class, "clawRot1");
+
         //separates initialization movement & start movement
         waitForStart();
+
+
+
+
+
+
+
 
         drive.setWeightedDrivePower(
                 new Pose2d(
