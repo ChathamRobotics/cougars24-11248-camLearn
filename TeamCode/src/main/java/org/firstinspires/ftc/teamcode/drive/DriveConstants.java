811686872ac51a4d10cfa9205e3206629ecd79d0
Cameron <Cameronfrancini@chatham-nj.org>
2024-01-24 09:09:01 -0500

    I continued modifying motor values to accurately reflect the robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -44,7 +44,7 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 2.95276; // in
+    public static double WHEEL_RADIUS = 1.47638; // in
     public static double GEAR_RATIO = 15.2; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 13.5; // in
 
@@ -65,7 +65,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
+    public static double MAX_VEL = 14100.0810682;
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
