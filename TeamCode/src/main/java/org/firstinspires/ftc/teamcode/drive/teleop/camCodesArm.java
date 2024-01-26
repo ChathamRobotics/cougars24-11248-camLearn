1984cd94a384ced1b1a85fb42a46916474dbecb9
Cameron <Cameronfrancini@chatham-nj.org>
2024-01-26 11:11:24 -0500

    The arm should now work... In theory...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/camCodesArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/camCodesArm.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp( name = "Cam Codes a Scuffed Teleop--> Arm")
@@ -17,16 +18,63 @@ public class camCodesArm extends LinearOpMode {
     DcMotorEx armPivot = null;
     private double basePower = 0.4;
 
+    private static final int MAX_PIVOT_POS = 4055;
 
+    private double armPivotPower = 1;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
 
-        armPivot = hardwareMap.get(DcMotorEx.class, "clawRot1");
+        armPivot = hardwareMap.get(DcMotorEx.class, "clawLift1");
+
+        armPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armPivot.setDirection(DcMotorSimple.Direction.REVERSE);
+        armPivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armPivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armPivot.setPower(0);
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
 
         //separates initialization movement & start movement
         waitForStart();
+        //drive code
+        if ((gamepad2.left_stick_y * armPivotPower * -1 > 0 && armPivot.getCurrentPosition() < MAX_PIVOT_POS - 50) || (gamepad2.left_stick_y * armPivotPower * -1 < 0 && armPivot.getCurrentPosition() > 100) || (!(armPivot.getCurrentPosition() <= 100) && gamepad2.left_stick_y != 0 && !(armPivot.getCurrentPosition() >= MAX_PIVOT_POS  - 50))) {
+            if (gamepad2.left_stick_y * -1 < 0) {
+                armPivot.setPower(0);
+            } else if (gamepad2.left_stick_y != 0) {
+                armPivot.setPower(gamepad2.left_stick_y * armPivotPower * -1);
+                armPivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                armPivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                telemetry.addData("currently moving", "clawLift1");
+                telemetry.addData("clawPivot Power", armPivot.getPower());
+            } else {
+                armPivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            }
+        } else if (armPivot.getCurrentPosition() >= 100 && armPivot.getCurrentPosition() <= MAX_PIVOT_POS - 50) {
+            armPivot.setPower(0);
+        } else if(armPivot.getCurrentPosition() < 100) {
+            if (armPivot.getCurrentPosition() < 0) {
+                armPivot.setPower(armPivotPower * 0.1);
+            } else {
+                armPivot.setPower(armPivotPower * 0.1 * -1);
+            }
+            armPivot.setTargetPosition(0);
+            armPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        } else if(armPivot.getCurrentPosition() > MAX_PIVOT_POS - 50) {
+            if (armPivot.getCurrentPosition() < MAX_PIVOT_POS) {
+                armPivot.setPower(MAX_PIVOT_POS * 0.1);
+            } else {
+                armPivot.setPower(armPivotPower * 0.1 * -1);
+            }
+            armPivot.setTargetPosition(MAX_PIVOT_POS);
+            armPivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        telemetry.addData("Claw Pivot Position", armPivot.getCurrentPosition());
+        telemetry.update();
 
 
 
@@ -34,7 +82,7 @@ public class camCodesArm extends LinearOpMode {
 
 
 
-
+            //this is the basic drive code
         drive.setWeightedDrivePower(
                 new Pose2d(
                         -gamepad1.left_stick_y * basePower,
